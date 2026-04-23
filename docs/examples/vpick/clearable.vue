f94265afeb7572e74859cf286e212897c48d4a25
syazwanz <66244514+syazwanz@users.noreply.github.com>
2026-04-23 12:58:30 +0800

    docs: add searchable/clearable docs and changeset

---
+++ docs/examples/vpick/clearable.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { VPick } from "vue-pick"
+
+const selected = ref<string | null>(null)
+const options = [
+  {
+    label: "North America",
+    options: [
+      { label: "Eastern Standard Time", value: "est" },
+      { label: "Central Standard Time", value: "cst" },
+      { label: "Mountain Standard Time", value: "mst" },
+      { label: "Pacific Standard Time", value: "pst" },
+      { label: "Alaska Standard Time", value: "akst" },
+      { label: "Hawaii Standard Time", value: "hst" },
+    ],
+  },
+  {
+    label: "Europe & Africa",
+    options: [
+      { label: "Greenwich Mean Time", value: "gmt" },
+      { label: "Central European Time", value: "cet" },
+      { label: "Eastern European Time", value: "eet" },
+      { label: "Western European Summer Time", value: "west" },
+      { label: "Central Africa Time", value: "cat" },
+      { label: "East Africa Time", value: "eat" },
+    ],
+  },
+  {
+    label: "Asia",
+    options: [
+      { label: "Moscow Time", value: "msk" },
+      { label: "India Standard Time", value: "ist" },
+      { label: "China Standard Time", value: "cst_china" },
+      { label: "Japan Standard Time", value: "jst" },
+      { label: "Korea Standard Time", value: "kst" },
+      { label: "Indonesia Central Standard Time", value: "ist_indonesia" },
+    ],
+  },
+  {
+    label: "Australia & Pacific",
+    options: [
+      { label: "Australian Western Standard Time", value: "awst" },
+      { label: "Australian Central Standard Time", value: "acst" },
+      { label: "Australian Eastern Standard Time", value: "aest" },
+      { label: "New Zealand Standard Time", value: "nzst" },
+      { label: "Fiji Time", value: "fjt" },
+    ],
+  },
+  {
+    label: "South America",
+    options: [
+      { label: "Argentina Time", value: "art" },
+      { label: "Bolivia Time", value: "bot" },
+      { label: "Brasilia Time", value: "brt" },
+      { label: "Chile Standard Time", value: "clt" },
+    ],
+  },
+]
+</script>
+
+<template>
+  <VPick
+    v-model="selected"
+    :options="options"
+    clearable
+    searchable
+    placeholder="Select a timezone"
+    style="--vpick-width: 17rem"
+    separators
+  />
+</template>
